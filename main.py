4e71499f645b2acb81f80defd4b1dba24370580e
Abdullox <yakubovabdulloh10@gmail.com>
2024-04-24 15:16:46 +0500

    drew respective scatterplots for each of the players; A stacked bar chart I.P.

--- main.py
+++ main.py
@@ -1,13 +1,8 @@
 import csv
 import matplotlib.pyplot as plt
 
-# user_input = input('Choose an option: \n1. Plots \n2. Analyze Data \n3. Exit \nEnter your choice: 1')
-# if user_input.isnumeric() == 1:
-#     user_input_2 = input('Choose an option: \n1. Scatter plot for Player 1 \n2. Scatter plot for Player 2 \n3. Scatter plot for Player 3 \n4. Scatter plot for all three players \n5. Stacked bar chart showing score distribution for each player \n6. Go back \nEnter your choice: ')
-
 with open ('dart_hits.csv', newline='') as csvfile:
     csv_reader = csv.reader(csvfile)
-    # list = [' '.join([element for element in row]) for row in csv_reader]
     list = []
     for row in csv_reader:
         for element in row:
@@ -30,3 +25,106 @@ player_1_y_coords = [float(y_coords[i]) for i in range(0, len(y_coords), 3)]
 player_2_y_coords = [float(y_coords[i]) for i in range(1, len(y_coords), 3)]
 
 player_3_y_coords = [float(y_coords[i]) for i in range(2, len(y_coords), 3)]
+
+user_input = input('Choose an option: \n1. Plots \n2. Analyze Data \n3. Exit \nEnter your choice: ')
+
+if int(user_input) == 1:
+    user_input_2 = input('Choose an option: \n1. Scatter plot for Player 1 \n2. Scatter plot for Player 2 \n3. Scatter plot for Player 3 \n4. Scatter plot for all three players \n5. Stacked bar chart showing score distribution for each player \n6. Go back \nEnter your choice: ')
+
+    if int(user_input_2) == 1:
+        fig, ax = plt.subplots()
+
+        ax.scatter(player_1_x_coords, player_1_y_coords, color = 'black', marker = '+')
+        
+        ax.set_title('Dart Hits for Player 1', pad = 15, color = 'red')
+        ax.set_xlabel('X', loc = 'right')
+        ax.set_ylabel('Y', loc = 'top', rotation = 'horizontal')
+        ax.axis([-4, 4, -4, 4])
+        ax.tick_params(colors = 'blue')
+        ax.xaxis.label.set_color('blue')
+        ax.yaxis.label.set_color('blue')
+        ax.spines['left'].set_color('black')
+        ax.spines['left'].set_position('zero')
+        ax.spines['bottom'].set_color('black')
+        ax.spines['bottom'].set_position('zero')
+        ax.spines['top'].set_color('none')
+        ax.spines['right'].set_color('none')
+
+        plt.show()
+
+    if int(user_input_2) == 2:
+        fig, ax = plt.subplots()
+
+        ax.scatter(player_2_x_coords, player_2_y_coords, color = 'green', marker = 'x')
+        
+        ax.set_title('Dart Hits for Player 2', pad = 15, color = 'red')
+        ax.set_xlabel('X', loc = 'right')
+        ax.set_ylabel('Y', loc = 'top', rotation = 'horizontal')
+        ax.axis([-4, 4, -4, 4])
+        ax.tick_params(colors = 'blue')
+        ax.xaxis.label.set_color('blue')
+        ax.yaxis.label.set_color('blue')
+        ax.spines['left'].set_color('black')
+        ax.spines['left'].set_position('zero')
+        ax.spines['bottom'].set_color('black')
+        ax.spines['bottom'].set_position('zero')
+        ax.spines['top'].set_color('none')
+        ax.spines['right'].set_color('none')
+
+        plt.show()
+
+    if int(user_input_2) == 3:
+        fig, ax = plt.subplots()
+
+        ax.scatter(player_3_x_coords, player_3_y_coords, color = 'blue', marker = 'o')
+        
+        ax.set_title('Dart Hits for Player 3', pad = 15, color = 'red')
+        ax.set_xlabel('X', loc = 'right')
+        ax.set_ylabel('Y', loc = 'top', rotation = 'horizontal')
+        ax.axis([-4, 4, -4, 4])
+        ax.tick_params(colors = 'blue')
+        ax.xaxis.label.set_color('blue')
+        ax.yaxis.label.set_color('blue')
+        ax.spines['left'].set_color('black')
+        ax.spines['left'].set_position('zero')
+        ax.spines['bottom'].set_color('black')
+        ax.spines['bottom'].set_position('zero')
+        ax.spines['top'].set_color('none')
+        ax.spines['right'].set_color('none')
+
+        plt.show()
+
+    if int(user_input_2) == 4:
+        fig, ax = plt.subplots()
+
+        ax.scatter(player_1_x_coords, player_1_y_coords, color = 'black', marker = '+')
+        ax.scatter(player_2_x_coords, player_2_y_coords, color = 'green', marker = 'x')
+        ax.scatter(player_3_x_coords, player_3_y_coords, color = 'blue', marker = 'o')
+        
+        ax.set_title('Dart Hits for All Players', pad = 15, color = 'red')
+        ax.set_xlabel('X', loc = 'right')
+        ax.set_ylabel('Y', loc = 'top', rotation = 'horizontal')
+        ax.axis([-4, 4, -4, 4])
+        ax.tick_params(colors = 'blue')
+        ax.xaxis.label.set_color('blue')
+        ax.yaxis.label.set_color('blue')
+        ax.spines['left'].set_color('black')
+        ax.spines['left'].set_position('zero')
+        ax.spines['bottom'].set_color('black')
+        ax.spines['bottom'].set_position('zero')
+        ax.spines['top'].set_color('none')
+        ax.spines['right'].set_color('none')
+
+        plt.show()
+
+    if int(user_input_2) == 5:
+        pass
+
+    if int(user_input_2) == 6:
+        pass
+
+if int(user_input) == 2:
+    pass
+
+if int(user_input) == 3:
+    pass
